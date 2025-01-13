bdc49edc377ec8bee9ec5d2e3e338458a3c68ebb
xhisham95 <xhisham95@gmail.com>
2025-01-14 04:00:13 +0800

    new file:   database/migrations/2025_01_13_194925_create_cache_table.php 	modified:   resources/views/users/report.blade.php

--- resources/views/users/report.blade.php
+++ resources/views/users/report.blade.php
@@ -10,3 +10,24 @@
     </form>
 </div>
 @endsection
+<h1>Users Report</h1>
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($users as $user)
+            <tr>
+                <td>{{ $user->User_ID }}</td>
+                <td>{{ $user->UserName }}</td>
+                <td>{{ $user->Email }}</td>
+                <td>{{ $user->Role }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
