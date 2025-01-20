63da987a6c8329df1e13cd58fe8695c36a4bc608
xhisham95 <xhisham95@gmail.com>
2025-01-20 18:21:22 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   app/Http/Controllers/UserController.php
    	modified:   app/Mail/UserCredentialsMail.php
    	modified:   resources/views/emails/user_credentials.blade.php

--- app/Mail/UserCredentialsMail.php
+++ app/Mail/UserCredentialsMail.php
@@ -26,36 +26,13 @@ class UserCredentialsMail extends Mailable
     }
 
     /**
-     * Get the message envelope.
+     * Build the message.
      *
-     * @return \Illuminate\Mail\Mailables\Envelope
+     * @return $this
      */
-    public function envelope(): \Illuminate\Mail\Mailables\Envelope
+    public function build()
     {
-        return new \Illuminate\Mail\Mailables\Envelope(
-            subject: 'Your FYP System Credentials'
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     *
-     * @return \Illuminate\Mail\Mailables\Content
-     */
-    public function content(): \Illuminate\Mail\Mailables\Content
-    {
-        return new \Illuminate\Mail\Mailables\Content(
-            view: 'emails.user_credentials'
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->subject('Your FYP System Credentials')
+            ->view('emails.user_credentials');
     }
 }
